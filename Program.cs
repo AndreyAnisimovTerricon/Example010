e89e984f8323a5d03a608aece376936c82947298
AndreyAnisimovTerricon <atriummebel79@yandex.ru>
2022-08-28 13:58:14 +0300

    Расстояние между двумя точками в 3D пространстве

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите Х1:");
+int x1 = int.Parse(Console.ReadLine());
+Console.Write("Введите Y1:");
+int y1 = int.Parse(Console.ReadLine());
+Console.Write("Введите Z1:");
+int z1 = int.Parse(Console.ReadLine());
+Console.Write("Введите X2:");
+int x2 = int.Parse(Console.ReadLine());
+Console.Write("Введите Y2:");
+int y2 = int.Parse(Console.ReadLine());
+Console.Write("Введите Z2:");
+int z2 = int.Parse(Console.ReadLine());
+Console.WriteLine(Math.Sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)+(z1-z2)*(z1-z2)));
